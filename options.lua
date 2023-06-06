1b01173f2f18dd9a3e90059d5ef56f9296fc1852
Tony Johnson <tony@tosp.net.au>
2023-06-06 19:47:59 +1000

    Added foldcolumn

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     colorcolumn = "80",
+    foldcolumn = "0",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader

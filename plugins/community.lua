f57bf8f2b5981648d9fde6e7978354a5e3e5a4a4
Tony Johnson <tony@tosp.net.au>
2023-09-24 19:45:25 +1000

    Added extra community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.python" },
